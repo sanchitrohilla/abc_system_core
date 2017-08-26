d2f5d0be86dddb6059238f4baa595e6ad2162080
xyyx <xyyx@mail.ru>
2017-08-27 02:17:09 +0800

    TEMP: Allow run services without selinux domain
    Signed-off-by: xyyx <xyyx@mail.ru>

--- init/service.cpp
+++ init/service.cpp
@@ -83,10 +83,6 @@ static std::string ComputeContextFromExecutable(std::string& service_name,
         computed_context = new_con;
         free(new_con);
     }
-    if (rc == 0 && computed_context == mycon.get()) {
-        LOG(ERROR) << "service " << service_name << " does not have a SELinux domain defined";
-        return "";
-    }
     if (rc < 0) {
         LOG(ERROR) << "could not get context while starting '" << service_name << "'";
         return "";
